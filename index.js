1cc28eb12d1fb9f1d8cd5d2f65d2476ebd5f978f
Juanma Menendez <juanmamenendez15@gmail.com>
2019-03-26 00:36:56 -0400

    - Refactor to OOP code

--- index.js
+++ index.js
@@ -1,56 +1,92 @@
 // ==UserScript==
 // @name         IHeartRadio shortcuts
 // @namespace    http://tampermonkey.net/
-// @version      0.3
+// @version      0.4
 // @description  IHeartRadio shortcuts: Play/Pause, Next song, Playlist relocation
 // @author       Juanma Menendez
 // @match        https://www.iheart.com/*
 // @grant        none
-// @downloadURL https://codepen.io/juanmamenendez15/pen/rRPPJd.js
-// @updateURL https://codepen.io/juanmamenendez15/pen/rRPPJd.js
+// @downloadURL  https://codepen.io/juanmamenendez15/pen/rRPPJd.js
+// @updateURL    https://codepen.io/juanmamenendez15/pen/rRPPJd.js
 // ==/UserScript==
 
-(function () {
-    'use strict';
+'use strict';
+
+class BaseShortcut {
+
+    constructor(keysArray, target, preventDefault = true, stopOtherHandlersChecking = true) {
+        this.keysArray = keysArray;
+        this.target = target;
+        this.preventDefault = preventDefault;
+        this.stopOtherHandlersChecking = stopOtherHandlersChecking;
+    }
+
+    action() {
+        console.log('CANCELARRRR ');
+    }
+}
 
 
-    document.addEventListener('keydown', function (ev) {
-        ev = ev || window.event; // for IE to cover IEs window object
+class ClickShortcut extends BaseShortcut {
 
-        //s || p || space :  play/pause
-        if (ev.key === 's' || ev.key === 'p' || ev.key === ' ') {
+    constructor(...params) {
+        super(...params);
+    }
 
-          let playButton = document.querySelector('div[data-test="mini-player-control-wrap"] button[data-test="play-button"]');
+    action() {
+        let node = document.querySelector(this.target);
 
-            if (playButton){
-                playButton.click();
-            }
-            else {
-                alert('Does not existe');
-            }
-
-            ev.preventDefault();
-
-            return false;
+        if (node) {
+            node.click();
         }
-
-        //n :  next song
-        if (ev.key === 'n') {
-            document.querySelector('div[data-test="mini-player-control-wrap"] button[data-test="skip-button"]').click();
-            return false;
+        else {
+            alert('Does not exist');
         }
+    }
+}
 
-        //f :  relocation to focus playlist
-        if (ev.key === 'f') {
-            window.location.href = "https://www.iheart.com/playlist/collections/perfect-for/activities/concentration/";
-            return false;
+
+class RelocationShortcut extends BaseShortcut {
+
+    constructor(...params) {
+        super(...params);
+    }
+
+    action() {
+        window.location.href = this.target;
+    }
+}
+
+
+let clickShortcutsArray = [
+    new ClickShortcut(' ', 'div[data-test="mini-player-control-wrap"] button[data-test="play-button"]'),
+    new ClickShortcut('n', 'div[data-test="mini-player-control-wrap"] button[data-test="skip-button"]'),
+];
+
+let relocationShortcutsArray = [
+    new RelocationShortcut('f', 'https://www.iheart.com/playlist/collections/perfect-for/activities/concentration/'),
+];
+
+
+function setListeners(...shortcutsArray) {
+
+    shortcutsArray.forEach((shortcut) => {
+
+            document.addEventListener('keydown', function (ev) {
+                ev = ev || window.event; // for IE to cover IEs window object
+
+                if (ev.key === shortcut.keysArray) {
+                    shortcut.action();
+                    ev.preventDefault();
+                    ev.stopImmediatePropagation();
+                    return false;
+                }
+
+            });
+
         }
+    )
 
-        //a :  test message
-        if (ev.key === 'a') {
-            window.alert("Update test 5");
-            return false;
-        }
+}
 
-    });
-})();
+setListeners(...clickShortcutsArray, ...relocationShortcutsArray);
